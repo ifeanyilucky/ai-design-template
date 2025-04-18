3877e650db0cf536cd5f5ad1e60a11b911bd925d
Ifeanyi Lucky <ifeanyilucky360@gmail.com>
2025-04-18 20:16:10 +0100

    Initial Commit

---
+++ AI Design R&D/AI_Design_R_DApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Design_R_DApp.swift
+//  AI Design R&D
+//
+//  Created by Great Codack's Macbook on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct AI_Design_R_DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Design R&DTests/AI_Design_R_DTests.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Design_R_DTests.swift
+//  AI Design R&DTests
+//
+//  Created by Great Codack's Macbook on 4/18/25.
+//
+
+import Testing
+@testable import AI_Design_R_D
+
+struct AI_Design_R_DTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
